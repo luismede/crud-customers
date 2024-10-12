922bfc43b12b735432aac4d62f24f679d5335213
luismede <luishenrique23hh23@gmail.com>
2024-10-12 20:23:36 -0300

    refactor: improved code readability and added try/except to improve code handling

--- app.py
+++ app.py
@@ -7,7 +7,13 @@ from colorama import init, Fore, Style
 
 init()
 
-while True:
+ADD_CUSTOMERS = 1
+VIEW_CUSTOMERS = 2
+DELETE_CUSTOMERS = 3
+UPDATE_CPF = 4
+UPDATE_NAME = 5
+
+def menu():
   print(f"\n{"-+-+"*5} CRUD Software {"-+-+"*5}")
   print("""
         1 - Adicionar novos clientes
@@ -16,18 +22,30 @@ while True:
         4 - Atualizar CPF
         5 - Atualizar nome
   """)
-  input_user = int(input("O que você deseja utilizar? "))
-  while input_user not in [1,2,3,4,5]:
-    print(Fore.RED + "\nOpção Inválida..." + Style.RESET_ALL)
-    input_user = int(input("O que você deseja utilizar? "))
+  try:
+    return int(input("O que você deseja utilizar? "))
+  except ValueError:
+    print(Fore.RED + "\nEntrada inválida. Por favor, insira um número." + Style.RESET_ALL)
+    return None
+
+def main():
+  while True:
+    input_user = menu()
     
-  if input_user == 1:
-    createCustomers()
-  elif input_user == 2:
-    readCustomers()
-  elif input_user == 3:
-    deleteCustomers()
-  elif input_user == 4:
-    updateCustomersCPF()
-  else:
-    updateCustomersName()
+    if input_user not in [1,2,3,4,5]:
+      print(Fore.RED + "\nOpção Inválida..." + Style.RESET_ALL)
+      continue
+
+    if input_user == ADD_CUSTOMERS:
+      createCustomers()
+    elif input_user == VIEW_CUSTOMERS:
+      readCustomers()
+    elif input_user == DELETE_CUSTOMERS:
+      deleteCustomers()
+    elif input_user == UPDATE_CPF:
+      updateCustomersCPF()
+    elif input_user == UPDATE_NAME:
+      updateCustomersName()
+      
+if __name__ == "__main__":
+  main()
