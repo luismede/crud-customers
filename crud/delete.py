9a0005a03e73e3a98eb0249149a92c4094a1634e
luismede <luishenrique23hh23@gmail.com>
2024-10-17 14:26:38 -0300

    feat: change ID for CPF to delete client

--- crud/delete.py
+++ crud/delete.py
@@ -6,16 +6,14 @@ init()
 def deleteCustomers():
     try:
         while True:
-            id_customer = int(input("\nDigite o ID do cliente que você deseja deletar: "))
+            cpf_customer = input("\nDigite o CPF do cliente que você deseja deletar: ")
             
-            confirmacao = input(Fore.YELLOW + f"Tem certeza que deseja deletar o cliente com ID {id_customer}? (s/n): " + Style.RESET_ALL).lower()
+            confirmacao = input(Fore.YELLOW + f"Tem certeza que deseja deletar o cliente com CPF {cpf_customer}? (s/n): " + Style.RESET_ALL).lower()
             if confirmacao.lower() == 's':
-                delete_customer(id_customer)
+                delete_customer(cpf_customer)
             else:
                 print(Fore.BLUE + "\nAção cancelada." + Style.RESET_ALL)
             break
-    except ValueError:
-        print(Fore.RED + "\nErro: O ID deve ser um número inteiro válido." + Style.RESET_ALL)
     except Exception as e:
         print(Fore.RED + f"\nErro ao deletar o cliente: {str(e)}" + Style.RESET_ALL)
 
